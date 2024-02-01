b67921cbdb97b6929ead05d8bfe2cae4f4ebc4b3
GinaKim <wls9416@gmail.com>
2024-02-01 13:35:02 +0900

    UPDATED: API

---
+++ ch.07/data_collection.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import pandas as pd
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+SERVICE_KEY = os.getenv('SEORUL_SERVICE_KEY')
+
+
+data = None
+for j in range(1,5):
+    url = f'http://openapi.seoul.go.kr:8088/{SERVICE_KEY}/json/tbLnOpendataRtmsV/{1+((j-1)*1000)}/{j*1000}'
+    # url = f'http://openapi.seoul.go.kr:8088/{service_key}/json/tbLnOpendataRentV/1/1000/2023/11560'
+    print(url)
+    req = requests.get(url)
+    content = req.json()
+    con = content['tbLnOpendataRtmsV']['row']
+    result = pd.DataFrame(con)
+    data = pd.concat([data, result])
+data = data.reset_index(drop=True)
+data
