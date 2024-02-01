d2d4abf278de13ccd906ad2ff7c29ed7d73b8006
GinaKim <wls9416@gmail.com>
2024-02-01 13:57:58 +0900

    UPDATED:API

--- ch.07/data_collection.py
+++ ch.07/data_collection.py
@@ -8,11 +8,9 @@ load_dotenv()
 
 SERVICE_KEY = os.getenv('SEORUL_SERVICE_KEY')
 
-
 data = None
 for j in range(1,5):
     url = f'http://openapi.seoul.go.kr:8088/{SERVICE_KEY}/json/tbLnOpendataRtmsV/{1+((j-1)*1000)}/{j*1000}'
-    # url = f'http://openapi.seoul.go.kr:8088/{service_key}/json/tbLnOpendataRentV/1/1000/2023/11560'
     print(url)
     req = requests.get(url)
     content = req.json()
@@ -20,4 +18,6 @@ for j in range(1,5):
     result = pd.DataFrame(con)
     data = pd.concat([data, result])
 data = data.reset_index(drop=True)
-data
+data.head()
+data.to_csv('./data/sample.csv', index=False)
+
